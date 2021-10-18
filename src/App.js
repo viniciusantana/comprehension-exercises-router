29ef1bcc84f00fc46237309d2ffb5cdea959f776
viniciusantana <viniciusantana@yahoo.com.br>
2021-10-18 19:18:27 -0300

    switch/broserRouter/rote=home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Home } />
+        </Switch>
+        <Home />
+      </BrowserRouter>
+      
     );
   }
 }
