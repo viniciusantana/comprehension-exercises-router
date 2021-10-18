a1e82dc8382a18162118fd6f7bb75153af5daefa
viniciusantana <viniciusantana@yahoo.com.br>
2021-10-18 19:22:41 -0300

    exercicio 1 a 04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import About from './components/About';
 import Home from './components/Home';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path="/about" component={ About } />
+          <Route path="/users" component={ Users } />
           <Route path="/" component={ Home } />
         </Switch>
-        <Home />
       </BrowserRouter>
       
     );
